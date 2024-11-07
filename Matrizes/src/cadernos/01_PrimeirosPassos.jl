0432e374c5034db8b6584ab6252f2e0bb0c48630
Gabriel Miglioranza <gabrimig@gmail.com>
2024-11-07 10:59:19 -0300

    Primeiras atualizações do caderno 01

---
+++ Matrizes/src/cadernos/01_PrimeirosPassos.jl
@@ -0,0 +1,115 @@
+### A Pluto.jl notebook ###
+# v0.20.0
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a577a2f8-9d09-11ef-18f1-25c98cdbf434
+md"""
+# Caderno 01 - Primeiros Passos
+"""
+
+# ╔═╡ 2ffb51f0-3f01-41ff-bd36-5167766a0db8
+md"""
+Antes de iniciar as atividades sobre Matrizes precisamos conhecer o ambiente de programação que vamos utilizar.
+
+Esse material que você está lendo foi desenvolvido no ambiente de programação [*Pluto*](https://plutojl.org/) desenvolvido utilizando a linguagem de programação [*Julia*](https://julialang.org/).
+
+Nesse caderno vamos mostrar alguns conceitos básicos da lingugem de programação, bem como, dicas de utilização do ambiente Pluto.
+"""
+
+# ╔═╡ 5170dff9-8fa9-425a-8b6b-48868582308b
+md"""
+## Operações Matemáticas
+
+Tal qual outras lingugens de programação *Julia* vem equipada com operações matemáticas básicas e funções elementárias. Portanto, podemos utilizar esse ambiente como uma calculadora.
+
+Para mais informações sobre operações matemáticas com *Julia* acessar: [https://docs.julialang.org/en/v1/manual/mathematical-operations/]()
+"""
+
+# ╔═╡ 6c1c6a0f-95ff-4dbf-b89a-891d5851c02b
+md"""
+### Adição
+Vamos fazer a nossa primeira conta no *Pluto*, na célula abaixo vamos escrever a soma de dois números, como exemplo, escreva na célula abaixo `2 + 3` e clique o símbolo de play abaixo da célula ou aperte shift+enter no seu teclado. O símbolo (+) representa a operação de adição.
+
+O comando shift+enter é um atalho para que o ambiente reproduza o que foi escrito na célula.
+"""
+
+# ╔═╡ acc18618-42b6-49f6-9fa7-b39740c54c97
+2 + 3
+
+# ╔═╡ a5ccfa0c-23f5-4aa6-a0fa-de5c329f1f9e
+md"""
+Após o fim da operação o número $$5$$ deve aparecer sobre a célula anterior.
+"""
+
+# ╔═╡ 75bfa1fc-5dd4-476e-9c0e-94c09203e992
+md"""
+### Subtração
+O operador menos (-) realiza subtrações. Tente utilizar `4 - 7` na célula a seguir.
+"""
+
+# ╔═╡ 3f753965-77a0-496b-9775-adb38172c617
+4 - 7
+
+# ╔═╡ 0dd30ba2-c6dc-4b2a-864f-3756284b0593
+md"""
+### Multiplicação
+Utilizamos o operador (`*`) para realizar operações de multiplicação. Tente utilizar ` 2 * 3` na célula a seguir.
+"""
+
+# ╔═╡ 4a9db253-356b-4239-a329-d9d4341a452e
+2 * 3
+
+# ╔═╡ 767395be-19b8-4541-a925-78503b4ac5c2
+md"""
+### Divisão
+Utilizamos o opertador (/) para realizar operações de divisão. Tente utilizar `15/3` na célula a seguir.
+"""
+
+# ╔═╡ 4d71765f-165f-45a0-a006-c221d57d8348
+15/3
+
+# ╔═╡ 555589ac-bf93-4c0e-8f32-a70157bebe73
+md"""
+### Potência
+"""
+
+# ╔═╡ 8c120ecc-7924-407d-857b-de4cd035d8b1
+md"""
+## Algumas Funções
+"""
+
+# ╔═╡ 1152b3f9-09d9-4add-8fff-3e6aa48e2e24
+md"""
+### Raíz Quadrada
+"""
+
+# ╔═╡ b717df53-8305-46ed-a330-b62b58856c45
+md"""
+### Função Exponencial
+"""
+
+# ╔═╡ 01e02f23-df6f-4092-8e71-f1c323db0652
+md"""
+### Função Seno e Cosseno
+"""
+
+# ╔═╡ Cell order:
+# ╟─a577a2f8-9d09-11ef-18f1-25c98cdbf434
+# ╟─2ffb51f0-3f01-41ff-bd36-5167766a0db8
+# ╟─5170dff9-8fa9-425a-8b6b-48868582308b
+# ╟─6c1c6a0f-95ff-4dbf-b89a-891d5851c02b
+# ╠═acc18618-42b6-49f6-9fa7-b39740c54c97
+# ╟─a5ccfa0c-23f5-4aa6-a0fa-de5c329f1f9e
+# ╟─75bfa1fc-5dd4-476e-9c0e-94c09203e992
+# ╠═3f753965-77a0-496b-9775-adb38172c617
+# ╟─0dd30ba2-c6dc-4b2a-864f-3756284b0593
+# ╠═4a9db253-356b-4239-a329-d9d4341a452e
+# ╠═767395be-19b8-4541-a925-78503b4ac5c2
+# ╠═4d71765f-165f-45a0-a006-c221d57d8348
+# ╟─555589ac-bf93-4c0e-8f32-a70157bebe73
+# ╟─8c120ecc-7924-407d-857b-de4cd035d8b1
+# ╟─1152b3f9-09d9-4add-8fff-3e6aa48e2e24
+# ╟─b717df53-8305-46ed-a330-b62b58856c45
+# ╟─01e02f23-df6f-4092-8e71-f1c323db0652
